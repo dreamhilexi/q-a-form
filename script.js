b12dd999d0c880e33e3735e22756933fa73d4110
Glitch (wooden-pine-triceratops) <none>
2023-02-04 22:11:38 +0000

    glitch

---
+++ script.js
@@ -0,0 +1,9 @@
+// allows the navigation bar to be responsive for mobile and desktop
+const navToggle = document.querySelector(".navbar_toggle");
+const links = document.querySelector(".main_nav");
+
+navToggle.addEventListener('click', function(){
+  links.classList.toggle("show_nav");
+})
+
+
